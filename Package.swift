cd52cf646714bc1c027f54362ea1352a3983ac2b
Muukii <muukii.app@gmail.com>
2022-01-11 15:56:43 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+  name: "Capturer",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(name: "Capturer", targets: ["Capturer"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "Capturer",
+      dependencies: [],
+      path: "Capturer"
+    ),
+  ]
+)
