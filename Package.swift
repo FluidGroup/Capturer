320d08971692e179454131a28ac8d7c0cbcd04f2
Muukii <muukii.app@gmail.com>
2023-03-03 15:36:32 +0900

    Patch

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "Capturer",
-  platforms: [.iOS(.v12)],
+  platforms: [.iOS(.v13)],
   products: [
     .library(name: "Capturer", targets: ["Capturer"]),
   ],
